142d4fea3ff7a02a5de40a858120c4be2ad05115
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2025-08-20 19:46:32 +0000

    Added initial code for day 4 in databases and typeorm

---
+++ databases_and_typeorm/day_4/src/database/seeders/curso-completo/etiqueta.entity.seed.ts
@@ -0,0 +1,25 @@
+import { DataSource } from 'typeorm';
+import { Etiqueta } from '@curso-completo/entities/etiqueta.entity';
+
+export class EtiquetaSeed {
+    public async run(dataSource: DataSource): Promise<void> {
+        const etiquetaRepo = dataSource.getRepository(Etiqueta);
+
+        const etiquetasBase = [
+            { nombre: 'Programación' },
+            { nombre: 'Bases de Datos' },
+            { nombre: 'Desarrollo Web' },
+            { nombre: 'Inteligencia Artificial' },
+            { nombre: 'DevOps' }
+        ];
+
+        for (const etiquetaData of etiquetasBase) {
+            const existe = await etiquetaRepo.findOne({ where: { nombre: etiquetaData.nombre } });
+            if (!existe) {
+                await etiquetaRepo.save(etiquetaData);
+            }
+        }
+
+        console.log(`Seed de etiquetas completo`);
+    }
+}
