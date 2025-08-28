7626734ba99cd768b08ec98a55ba539fb3cdcf12
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2025-08-28 23:07:51 +0000

    Removed AI tests from day 2

--- microservices_and_event_drive_arch/day_2/tests/mock/cache-manager.mock.ts
+++
@@ -1,7 +0,0 @@
-import { Cache } from "cache-manager";
-
-export const mockCacheManager: Cache = {
-    get: jest.fn().mockResolvedValue(undefined),
-    set: jest.fn().mockResolvedValue(undefined),
-    del: jest.fn().mockResolvedValue(undefined),
-} as any;
