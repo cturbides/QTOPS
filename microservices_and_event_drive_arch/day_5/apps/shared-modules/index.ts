c374b55bb3023c3948a66dde32396950c3bd31c3
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2025-09-02 17:33:59 +0000

    Added day 5 solution (microservices and event drive architecture)

--- microservices_and_event_drive_arch/day_5/apps/shared-modules/index.ts
+++ microservices_and_event_drive_arch/day_5/apps/shared-modules/index.ts
@@ -10,6 +10,15 @@ export { ServiceDiscoveryModule } from './service-discovery/service-discovery.mo
 // Versioning Module
 export * from './versioning';
 
-// Observability Module
-export * from './observability';
+// Observability exports
+export { ObservabilityModule } from './observability/observability.module';
+export { DistributedLogger } from './observability/distributed-logger.service';
+export { PrometheusMetricsService } from './observability/prometheus-metrics.service';
+export { TracingService } from './observability/tracing.service';
+export { ELearningObservabilityService } from './observability/elearning-observability.service';
+export { CorrelationMiddleware } from './observability/correlation.middleware';
+export { MetricsInterceptor } from './observability/metrics.interceptor';
+export { AnomalyDetectionService, EnrollmentAnomaly } from './observability/anomaly-detection.service';
+export { AlertNotificationService, AlertNotification } from './observability/alert-notification.service';
+export { EnrollmentAlertingService } from './observability/enrollment-alerting.service';
 
--- microservices_and_event_drive_arch/day_5/apps/shared-modules/observability/observability.module.ts
+++ microservices_and_event_drive_arch/day_5/apps/shared-modules/observability/observability.module.ts
@@ -1,13 +1,19 @@
 import { Module, Global } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { DistributedLogger } from './distributed-logger.service';
 import { TracingService } from './tracing.service';
 import { PrometheusMetricsService } from './prometheus-metrics.service';
 import { ELearningObservabilityService } from './elearning-observability.service';
 import { CorrelationMiddleware } from './correlation.middleware';
 import { MetricsInterceptor } from './metrics.interceptor';
+import { AnomalyDetectionService } from './anomaly-detection.service';
+import { AlertNotificationService } from './alert-notification.service';
+import { EnrollmentAlertingService } from './enrollment-alerting.service';
+import { AlertingController } from './alerting.controller';
 
 @Global()
 @Module({
+  imports: [ScheduleModule.forRoot()],
   providers: [
     DistributedLogger,
     TracingService,
@@ -15,7 +21,11 @@ import { MetricsInterceptor } from './metrics.interceptor';
     ELearningObservabilityService,
     CorrelationMiddleware,
     MetricsInterceptor,
+    AnomalyDetectionService,
+    AlertNotificationService,
+    EnrollmentAlertingService,
   ],
+  controllers: [AlertingController],
   exports: [
     DistributedLogger,
     TracingService,
@@ -23,6 +33,9 @@ import { MetricsInterceptor } from './metrics.interceptor';
     ELearningObservabilityService,
     CorrelationMiddleware,
     MetricsInterceptor,
+    AnomalyDetectionService,
+    AlertNotificationService,
+    EnrollmentAlertingService,
   ],
 })
 export class ObservabilityModule {}
