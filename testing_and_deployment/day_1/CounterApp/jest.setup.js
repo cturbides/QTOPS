0121b90167690a7ecf796d0665b112c46f12d280
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2025-10-29 03:53:27 +0000

    Added day 1 solution of testing and deployment

---
+++ testing_and_deployment/day_1/CounterApp/jest.setup.js
@@ -0,0 +1,59 @@
+// Mock react-native-gesture-handler
+jest.mock('react-native-gesture-handler', () => {
+  const View = require('react-native/Libraries/Components/View/View');
+  return {
+    Swipeable: View,
+    DrawerLayout: View,
+    State: {},
+    ScrollView: View,
+    Slider: View,
+    Switch: View,
+    TextInput: View,
+    ToolbarAndroid: View,
+    ViewPagerAndroid: View,
+    DrawerLayoutAndroid: View,
+    WebView: View,
+    NativeViewGestureHandler: View,
+    TapGestureHandler: View,
+    FlingGestureHandler: View,
+    ForceTouchGestureHandler: View,
+    LongPressGestureHandler: View,
+    PanGestureHandler: View,
+    PinchGestureHandler: View,
+    RotationGestureHandler: View,
+    RawButton: View,
+    BaseButton: View,
+    RectButton: View,
+    BorderlessButton: View,
+    FlatList: View,
+    gestureHandlerRootHOC: jest.fn(),
+    Directions: {},
+  };
+});
+
+// Mock AsyncStorage
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+);
+
+// Mock React Navigation
+jest.mock('@react-navigation/native', () => ({
+  useNavigation: () => ({
+    navigate: jest.fn(),
+    goBack: jest.fn(),
+  }),
+  useRoute: () => ({
+    params: {},
+  }),
+  NavigationContainer: ({ children }) => children,
+}));
+
+jest.mock('@react-navigation/stack', () => ({
+  createStackNavigator: jest.fn(() => ({
+    Navigator: ({ children }) => children,
+    Screen: () => null,
+  })),
+}));
+
+// Global test utilities
+global.fetch = jest.fn();
